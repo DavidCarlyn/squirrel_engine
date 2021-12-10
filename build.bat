91d39083aea6acc6e11dbf8fdb58b56e32024a4c
DavidCarlyn <davidecarlyn@gmail.com>
2021-12-10 15:40:41 -0500

    Restarting

--- build.bat
+++
@@ -1,5 +0,0 @@
-cd build
-make
-cd ..\apps
-g++ main.cpp -I ..\dependencies\glad\include -I ..\dependencies\glfw\include -I ..\src\include -L ..\build\dependencies\glad -lglad -L ..\build\dependencies\glfw\src -lglfw3 -L ..\build -lsquirrel_engine -lgdi32 -lopengl32 -o app
-cd ..\
